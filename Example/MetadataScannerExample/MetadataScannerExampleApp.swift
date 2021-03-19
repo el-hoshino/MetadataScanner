a5fe2b928d45c2ae5a9df0a9f9b8771dee77cb9f
史 翔新 <x_shi@yumemi.co.jp>
2021-03-19 15:19:54 +0900

    Add an example app to demostrate how to use this library

---
+++ Example/MetadataScannerExample/MetadataScannerExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetadataScannerExampleApp.swift
+//  MetadataScannerExample
+//
+//  Created by 史 翔新 on 2021/03/18.
+//
+
+import SwiftUI
+
+@main
+struct MetadataScannerExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
