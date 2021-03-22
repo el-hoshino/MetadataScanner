a299fa2aa67875e4ede85b9b37dddecb9d5a4baf
史 翔新 <x_shi@yumemi.co.jp>
2021-03-22 15:24:19 +0900

    Add documents for public types and methods

--- Sources/MetadataScanner/MetadataScanner.swift
+++ Sources/MetadataScanner/MetadataScanner.swift
@@ -9,6 +9,9 @@
 
 import SwiftUI
 
+/// `MetadataScanner` is a `Button`-like `SwiftUI.View` component.
+/// You can use it anywhere in your `body: some View` property in a `View` just like any other `View` component.
+/// It will call `onScannedObjectUpdate` callback when found any metadata, just like a `Button` callback when tapped.
 public struct MetadataScanner: View {
     
     private static let sharedEngine: ScannerEngine? = try? .init()
@@ -20,6 +23,19 @@ public struct MetadataScanner: View {
     
     var onScannedObjectUpdate: (([ScannedMetadataObject]?) -> Void)
     
+    /// MetadataScanner initializer.
+    /// Please note that this is a failable initializer and it will be nil if it can't find any usable camera.
+    /// And in that case, you may want to create another placeholder view.
+    ///
+    /// - Parameters:
+    ///   - videoGravity: `VideoGravity` is actually a type alias of `AVLayerVideoGravity`.
+    ///     You may set it with any of `.resize`, `.resizeAspect` or `.resizeAspectFill`.
+    ///   - objectTypes: `ScannableObjectType` is actually a type alias of `AVMetadataObject.ObjectType`.
+    ///     There are many available object types, including the most famous `.qr` for reading QR codes.
+    ///   - scans: This property controls whether the camera should work or not.
+    ///     Please note that if you are writing the scan result to a `@State` value, you may want to set it to `false` until the view did appear, otherwise SwiftUI will throw a warning of "Modifying state during view update" at runtime.
+    ///   - onScannedObjectUpdate: A callback of `([ScannedMetadataObject]?) -> Void)`, when `MetadataScanner` initialized or found any result from camera.
+    ///     `AVMetadataMachineReadableCodeObject` is actually a type alias of `AVMetadataMachineReadableCodeObject`.
     public init?(videoGravity: VideoGravity, objectTypes: [ScannableObjectType], scans: Bool, onScannedObjectUpdate: @escaping ([ScannedMetadataObject]?) -> Void) {
         guard let engine = Self.sharedEngine else {
             return nil
